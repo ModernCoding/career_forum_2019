d47faa406cc79705da39305b3e0d535f4b355f4c
Michel Boretti <michel.boretti@pnc.lan>
2019-08-23 19:39:03 +0700

    Links and keys

--- js/app.js
+++ js/app.js
@@ -6,3 +6,59 @@ function clock() {
 
 clock();
 setInterval(clock, 1000);
+
+for (element of document.getElementsByTagName('body')) {
+
+  element.addEventListener('keyup', function(e){
+
+    switch (e.keyCode) {
+
+      case 49:
+
+        if (document.getElementsByClassName('my-guest-1').length > 0) {
+          document.getElementsByClassName('my-guest-1')[0].click();
+        }
+
+        break;
+
+        
+      case 50:
+        
+        if (document.getElementsByClassName('my-guest-2').length > 0) {
+          document.getElementsByClassName('my-guest-2')[0].click();
+        }
+        
+        break;
+        
+
+      case 51:
+
+        if (document.getElementsByClassName('my-guest-3').length > 0) {
+          document.getElementsByClassName('my-guest-3')[0].click();
+        }
+      
+        break;
+      
+
+      case 52:
+
+        if (document.getElementsByClassName('my-guest-4').length > 0) {
+          document.getElementsByClassName('my-guest-4')[0].click();
+        }
+      
+        break;
+      
+
+      default:
+
+        if (document.getElementsByClassName('my-index').length > 0) {
+          document.getElementsByClassName('my-index')[0].click();
+        }
+
+        break;
+
+    }
+
+  });
+
+}
