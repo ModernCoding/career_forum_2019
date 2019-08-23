089f50982ac35a1a07d5bf1730b8e552c7fe5e47
Michel Boretti <michel.boretti@pnc.lan>
2019-08-23 19:59:23 +0700

    Intro

--- js/app.js
+++ js/app.js
@@ -1,7 +1,6 @@
 function clock() {
-  myClock = document.getElementById('my-clock');
-  var today = new Date();
-  myClock.innerText = ("0" + today.getHours()).slice(-2) + " : " + ("0" + today.getMinutes()).slice(-2) + " : " + ("0" + today.getSeconds()).slice(-2);
+  let today = new Date();
+  document.getElementById('my-clock').innerText = ("0" + today.getHours()).slice(-2) + " : " + ("0" + today.getMinutes()).slice(-2) + " : " + ("0" + today.getSeconds()).slice(-2);
 }
 
 clock();
